4cd542276b3650285c88880a4579ca759d17d0a9
mike macfarland <m_macfarland@live.com>
2023-04-05 16:16:18 -0400

    initial commit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+#Webpack website
+
+##dev dependencies
+    - css-loader
+    - html-webpack-plugin
+    - live-server
+    - mini-css-extract-plugin
+    - style-loader
+    - webpack
+    - webpack-cli
+    - webpack-dev-server
+
+##Instructions on how to install and run the project locally.
+    - Clone repo
+    - Cd into folder
+    - run `npm i`
+    - run `npm preview` to run build and preview in browser
+    - run `npm start` to start development server
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
